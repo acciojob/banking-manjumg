bac29aa0adf04b181e0ffb23274e861dd33ac8d2
Manjunath M Goni <manjugoni007@gmail.com>
2023-12-16 17:19:20 +0530

    Update StudentAccount.java

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -6,7 +6,31 @@ public class StudentAccount extends BankAccount{
 
     public StudentAccount(String name, double balance, String  institutionName) {
         //minimum balance is 0 by default
-
+        super(name,balance);
+        this.institutionName=institutionName;
+        this.setMinBalance(0);
     }
 
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public double getBalance() {
+        return super.getBalance();
+    }
+
+    @Override
+    public double getMinBalance() {
+        return super.getMinBalance();
+    }
+
+    public String getInstitutionName() {
+        return institutionName;
+    }
+
+    public void setInstitutionName(String institutionName) {
+        this.institutionName = institutionName;
+    }
 }
