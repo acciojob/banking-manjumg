8f4e8f3f2914b5ebe440c89f5ce16a3186b224a3
Manjunath M Goni <manjugoni007@gmail.com>
2023-12-16 17:17:29 +0530

    Update CurrentAccount.java

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,11 +1,26 @@
 package com.driver;
 
+import java.util.Stack;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name,balance);
+        this.tradeLicenseId=tradeLicenseId;
+        this.setMinBalance(5000);
+        if(this.getBalance()<this.getMinBalance()){
+            throw new Exception("Insufficient Balance");
+        }
+    }
 
+    public String getTradeLicenseId() {
+        return tradeLicenseId;
+    }
+
+    public void setTradeLicenseId(String tradeLicenseId) {
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -14,6 +29,13 @@ public class CurrentAccount extends BankAccount{
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
+        for(int i=0;i<this.tradeLicenseId.length()-1;i++){
+            char c1=tradeLicenseId.charAt(i);
+            char c2=tradeLicenseId.charAt(i+1);
+            if(c1==c2){
+                throw new Exception("Valid License can not be generated");
+            }
+        }
     }
 
 }
